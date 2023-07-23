f055888e01773ecd21cece5a0b745aa29810f289
Kabir <seenhubble@gmail.com>
2023-07-24 02:47:13 +0530

    front end almost complete

--- src/app/Components/education/education.component.ts
+++ src/app/Components/education/education.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent {
+  education = [
+    {
+      'heading': 'Secondary Class',
+      'institution': "Kendriya Vidyalaya No.1 Salt Lake, Kolkata",
+      'degree': "Xth",
+      'icon': 'dev-icon fa-solid fa-school'
+    },
+    {
+      'heading': 'Sr Secondary Class',
+      'institution': "Midfields Sr Sec School, Delhi",
+      'degree': "XIIth",
+      'icon': 'dev-icon fa-solid fa-building-columns'
+    },
+    {
+      'heading': 'Graduation',
+      'institution': "Delhi Technological University",
+      'degree': "B.Tech",
+      'icon': 'dev-icon fa-solid fa-graduation-cap'
+    }
+  ]
 
 }
